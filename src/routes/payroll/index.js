a4a122246315a97a032c8b7a9fa4db642e729a0e
Andrew Dourgarian <andrewdourgarian@yahoo.com>
2017-02-11 15:15:19 -0600

    changed page title

--- src/routes/payroll/index.js
+++ src/routes/payroll/index.js
@@ -11,7 +11,7 @@ import React from 'react';
 import Layout from '../../components/Layout';
 import Payroll from './Payroll';
 
-const title = 'Contact Us';
+const title = 'Syscoin Payroll';
 
 export default {
 
